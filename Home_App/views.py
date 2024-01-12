82401379aeaf25788ed52861b0814d97430a4067
Tusharsharma2305 <2305tushar@gmail.com>
2024-01-12 09:39:12 +0530

    order

--- Home_App/views.py
+++ Home_App/views.py
@@ -16,6 +16,11 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import get_user_model
+import json
+from django.shortcuts import render, redirect
+from django.views import View
+from django.core.mail import send_mail
+from .models import MenuItem, Category, OrderModel
 
 
 # Create your views here.
@@ -388,6 +393,120 @@ def add_foodItems(request):
             return redirect(reverse('admin-dashboard-section', kwargs={'section':'foodItems'}))
         
     return render(request,'admin-dashboard-foodItems-add.html',{'form':form})
+
+# order
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(
+            category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        # pass into context
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+
+        # render the template
+        
+        return render(request, 'order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
+        return render(request, 'order_confirmation.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        data = json.loads(request.body)
+
+        if data['isPaid']:
+            order = OrderModel.objects.get(pk=pk)
+            order.is_paid = True
+            order.save()
+
+        return redirect('payment-confirmation')
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'order_pay_confirmation.html')
+
 
 
 #  ----- Reservation -----
